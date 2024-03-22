8191fe2aed64efc1577e1a62ff05829c2f0ab56b
iroid <iradtu2@gmail.com>
2024-03-22 12:30:35 +0300

    js

--- components/Settings.tsx
+++ components/Settings.tsx
@@ -7,7 +7,7 @@ export default function Settings() {
         <p>Our website stores cookies on your device and discloses information in accordance with our Cookie Statement. Choose "Customize Settings" to control cookies. We may collect certain aggregate and anonymized data from your browser independent of your cookie preferences. Cookie Statement</p>
         <div className="">
           <Link  href={''}>Customise Settings</Link>
-          <h1 className='text-3xl'>X</h1>
+          <h1 className='text-3xl'>this an ex</h1>
         </div>
     </div>
   )
